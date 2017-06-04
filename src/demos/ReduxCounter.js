a3264421ed388048213754b3fb63b33e87959932
John Paxton <pax@speedingplanet.com>
2017-06-03 23:07:27 -0400

    New demo!

---
+++ src/demos/ReduxCounter.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import {createStore} from 'redux';
+
+// Reducer
+const reducer = ( state = 0, action ) => {
+  console.log( 'Called reducer' );
+  switch ( action.type ) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+// Actions
+const addOne = () => {
+  return {
+    type: 'INCREMENT'
+  }
+};
+
+const subtractOne = () => {
+  return {
+    type: 'DECREMENT'
+  }
+};
+
+// Creating the store
+const store = createStore( reducer );
+
+// Components
+const Counter = ( { value, onIncrement, onDecrement } ) => {
+  console.log( 'Counter.render() called with value: ', value );
+  return (
+    <div>
+      <h3>{value}</h3>
+      <button onClick={onIncrement}>Add one</button>
+      <button onClick={onDecrement}>Subtract one</button>
+    </div>
+  )
+};
+
+class ReduxCounter extends Component {
+  constructor( props ) {
+    super( props );
+
+    // Working with the store
+    this.state = {
+      value: store.getState()
+    };
+
+    store.subscribe( () => this.setState( {
+      value: store.getState()
+    } ) );
+  }
+
+  render() {
+    console.log( 'ReduxCounter.render() called, state is ', store.getState() );
+    return (
+      <div>
+        <h3>Redux Counter</h3>
+        {/* Dispatching actions */}
+        <Counter value={this.state.value}
+                 onIncrement={() => store.dispatch( addOne() )}
+                 onDecrement={() => store.dispatch( subtractOne() )}/>
+      </div>
+    )
+  }
+}
+
+export default ReduxCounter;
