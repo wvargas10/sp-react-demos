bc36ed4840fc1b1219fdade5e79f32acb109f148
John Paxton <pax@speedingplanet.com>
2017-09-22 02:22:52 -0400

    Formatting differences

--- src/demos/TreeComponent.js
+++ src/demos/TreeComponent.js
@@ -1,20 +1,30 @@
 import React from 'react';
 
 const TreeComponent = ( props ) => {
-  let colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+  let colors = ['red', 'orange', 'yellow',
+    'green', 'blue', 'indigo', 'violet'];
 
   // You can get rid of one of these to break the snapshot
-  let fruits = ['banana', 'apple', 'orange', 'lemon', 'lime', 'pineapple', 'plum', 'pluot', 'kiwi', 'dragonfruit'];
+  let fruits = ['banana', 'apple', 'orange',
+    'lemon', 'lime', 'pineapple', 'plum', 'pluot',
+    'kiwi', 'dragonfruit'];
 
   function getColor(current) {
-    return colors[(current + 1) % 7];
+    return colors[(current) % 7];
   }
 
   return (
     <div>
       <ul>
         {
-          fruits.map( (thing, count) => <li key={count} style={{color: getColor(count-1)}}>{thing}</li>)
+          fruits.map( (thing, count) => {
+            return (
+              <li key={count}
+                  style={{color: getColor(count + 3)}}>
+                {thing}
+                </li>
+            )
+          })
         }
       </ul>
     </div>
