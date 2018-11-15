cf539237553f80b0ed2cf242de189ef56d7fafe5
John Paxton <pax@speedingplanet.com>
2018-11-15 01:31:14 -0500

    npm test fixes (CustomDropDown in particular)

--- src/demos/ReduxCounter.js
+++ src/demos/ReduxCounter.js
@@ -1,19 +1,6 @@
 import React, {Component} from 'react';
 import {createStore} from 'redux';
 
-// Reducer
-const reducer = ( state = 0, action ) => {
-  console.log( 'Called reducer' );
-  switch ( action.type ) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
 // Actions
 const addOne = () => {
   return {
@@ -24,6 +11,19 @@ const addOne = () => {
 const subtractOne = () => {
   return {
     type: 'DECREMENT'
+  }
+};
+
+// Reducer
+const reducer = ( state = 0, action ) => {
+  console.log( 'Called reducer' );
+  switch ( action.type ) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
   }
 };
 
--- src/tests/CustomDropDown.test.js
+++ src/tests/CustomDropDown.test.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import CustomDropDown from '../CustomDropDown';
-import {peopleDAO} from '../data/class-data';
+import { peopleDAO } from '../data/class-data';
+
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({adapter: new Adapter()});
 
 let personList;
 
@@ -12,7 +15,7 @@ beforeEach( () => {
 
 test( 'CustomDropDown single labels render correctly', () => {
   const dropDown = shallow(
-    <CustomDropDown idField="id" label="lastName" options={personList}/>
+    <CustomDropDown idField="id" label="lastName" options={ personList }/>
   );
 
   expect( dropDown.text() ).toMatch( new RegExp( personList[ 0 ].lastName ) );
@@ -23,7 +26,7 @@ test( 'CustomDropDown single labels render correctly', () => {
 
 test( 'CustomDropDown multiple labels render correctly', () => {
   const dropDown = shallow(
-    <CustomDropDown idField="id" label="firstName lastName" options={personList}/>
+    <CustomDropDown idField="id" label="firstName lastName" options={ personList }/>
   );
 
   expect( dropDown.text() ).toMatch( new RegExp( `${personList[ 0 ].firstName} ${personList[ 0 ].lastName}` ) );
@@ -34,43 +37,43 @@ test( 'CustomDropDown multiple labels render correctly', () => {
 
 it( 'CustomDropDown selections work', () => {
   const dropDown = shallow(
-    <CustomDropDown idField="id" label="lastName" options={personList}/>
+    <CustomDropDown idField="id" label="lastName" options={ personList }/>
   );
 
   const select = dropDown.find( 'select' );
   select.simulate( 'change', { target: { value: '203' } } );
 
-  expect(dropDown.state('value')).toEqual('203');
+  expect( dropDown.state( 'value' ) ).toEqual( '203' );
 
 } );
 
-it('CustomDropDown defaultValue test', () => {
+it( 'CustomDropDown defaultValue test', () => {
   const dropDown = shallow(
-    <CustomDropDown idField="id" label="lastName" options={personList} defaultValue="203"/>
+    <CustomDropDown idField="id" label="lastName" options={ personList } defaultValue="203"/>
   );
 
-  expect(dropDown.state('value')).toEqual('203');
-});
+  expect( dropDown.state( 'value' ) ).toEqual( '203' );
+} );
 
 // Snapshot test
-it('CustomDropDown snapshot test', () => {
+it( 'CustomDropDown snapshot test', () => {
   const tree = renderer
-    .create(<CustomDropDown idField="id" label="lastName" options={personList} defaultValue="203"/>)
+    .create( <CustomDropDown idField="id" label="lastName" options={ personList } defaultValue="203"/> )
     .toJSON();
 
-  expect(tree).toMatchSnapshot();
+  expect( tree ).toMatchSnapshot();
 
-});
+} );
 
-it('CustomDropDown placeholder snapshot', () => {
+it( 'CustomDropDown placeholder snapshot', () => {
   const tree = renderer
-    .create(<CustomDropDown idField="id"
-                            label="lastName"
-                            options={personList}
-                            defaultValue="203"
-                            placeholder="Select a person"/>)
+    .create( <CustomDropDown idField="id"
+                             label="lastName"
+                             options={ personList }
+                             defaultValue="203"
+                             placeholder="Select a person"/> )
     .toJSON();
 
-  expect(tree).toMatchSnapshot();
+  expect( tree ).toMatchSnapshot();
 
-});
+} );
